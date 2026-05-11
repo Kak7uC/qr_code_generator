92a6ed56ace563d35d4d961da1576db7d0db05dc
Nuzet <nuzet@inbox.ru>
2026-05-11 21:14:16 +0300

    Change QR code data

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+@'
 import qrcode
-img = qrcode.make('Some data here')
-img.save("some_file.png")
+img = qrcode.make('Other data here')
+img.save("other_file.png")
+'@ | Set-Content -Encoding UTF8 main.py
